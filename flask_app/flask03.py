72e41421bcbb2b7f303adefaa632c2a714ca2ff2
Chase Scallion <cscallio@uncc.edu>
2020-12-11 00:30:25 -0500

    flask app #3 - added a new note route and view

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -34,6 +34,10 @@ def get_note(note_id):
 
     return render_template('note.html', note=notes[int(note_id)])
 
+@app.route('/notes/new')
+def new_note():
+    a_user = {'name': 'Chase', 'email': 'cscallio@uncc.edu'}
+    return render_template('new.html', user=a_user)
 
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000/
