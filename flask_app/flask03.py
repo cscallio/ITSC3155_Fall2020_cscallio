a7452e3315d59d419cbe3c8b971bebc4e4acf088
Chase Scallion <cscallio@uncc.edu>
2020-12-11 00:36:41 -0500

    flask app #3 - added personalized header to all views

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -14,25 +14,27 @@ app = Flask(__name__)     # create an app
 def index():
     a_user = {'name': 'Chase', 'email': 'cscallio@uncc.edu'}
 
-    return render_template('index.html' , user = a_user)
+    return render_template('index.html', user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 @app.route('/notes')
 def get_notes():
+    a_user = {'name': 'Chase', 'email': 'cscallio@uncc.edu'}
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}, 2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
 
-    return render_template('notes.html', notes=notes)
+    return render_template('notes.html', notes=notes, user=a_user)
 
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
+    a_user = {'name': 'Chase', 'email': 'cscallio@uncc.edu'}
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}, 2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
 
-    return render_template('note.html', note=notes[int(note_id)])
+    return render_template('note.html', note=notes[int(note_id)], user=a_user)
 
 @app.route('/notes/new')
 def new_note():
