dbfe5b22e9c591b5f5c6690e22c81a1a651e7367
Chase Scallion <cscallio@uncc.edu>
2020-12-11 12:42:32 -0500

    login registration functionality and added nav bars

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -2,17 +2,24 @@
 
 # imports
 import os  # os is used to get environment variables IP & PORT
+import bcrypt
+
 from flask import Flask  # Flask is the web app that we will customize
+from flask import redirect, url_for
 from flask import render_template
 from flask import request
-from flask import redirect, url_for
+from flask import session
+
 from database import db
+from forms import RegisterForm
+from forms import LoginForm
 from models import Note as Note
 from models import User as User
 
 app = Flask(__name__)  # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'SE3155'
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
 # Setup models
@@ -37,11 +44,14 @@ app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), de
 @app.route('/notes')
 def get_notes():
     # get user from database
-    a_user = db.session.query(User).filter_by(email='cscallio@uncc.edu').one()
-    # get notes from database
-    my_notes = db.session.query(Note).all()
+    # check if a user is saved in session
+    if session.get('user'):
+        # get notes from database
+        my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
 
-    return render_template('notes.html', notes=my_notes, user=a_user)
+        return render_template('notes.html', notes=my_notes, user=session['user'])
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/notes/<note_id>')
@@ -116,6 +126,55 @@ def delete_note(note_id):
     db.session.commit()
 
     return redirect(url_for('get_notes'))
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+    # validate_on_submit only validates using POST
+    if form.validate_on_submit():
+        # form validation included a criteria to check the username does not exist
+        # we can know we are safe to add the user to the database
+        password_hash = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        new_record = User(first_name, last_name, request.form['email'], password_hash)
+        db.session.add(new_record)
+        db.session.commit()
+        # save the user's name to the session
+        session['user'] = first_name
+        # get the id of the newly created database record
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
+        # save the user's id to the session
+        session['user_id'] = the_user.id
+
+        return redirect(url_for('get_notes'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    login_form = LoginForm()
+    # validate_on_submit only validates using POST
+    if login_form.validate_on_submit():
+        # we know user exists. We can use one()
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
+        # user exists check password entered matches stored password
+        if bcrypt.checkpw(request.form['password'].encode('utf-8'), the_user.password):
+            # password match add user info to session
+            session['user'] = the_user.first_name
+            session['user_id'] = the_user.id
+            # render view
+            return redirect(url_for('get_notes'))
+
+        # password check failed
+        # set error message to alert user
+        login_form.password.errors = ["Incorrect username or password."]
+        return render_template("login.html", form=login_form)
+    else:
+        # form did not validate or GET request
+        return render_template("login.html", form=login_form)
 
 
 # To see the web page in your web browser, go to the url,
